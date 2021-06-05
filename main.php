2c57e443197d443c3110306e4e553a895ab1944c
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2021-06-05 22:58:29 +0530

    Create main.php

---
+++ main.php
@@ -0,0 +1,29 @@
+<?
+include('conn.php');
+?>
+<html>
+<head>
+    <meta charset="utf-8">
+     <title>Home Page</title>
+	 <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class = "box2">
+
+
+<form action = "admin_login.php" method "post">
+
+<input type = "submit" name = "submit1" value = " Admin  Login">
+</form>
+
+<form action = "register.php" method = "post">
+<input type = "submit" name = "submit2" value = "Student Register"><br>
+</form>
+</div>
+
+<div class = "boxsp">
+
+<h1>Welcome to Placement Record Management System</h1>
+</div>
+</body>
+</html>
